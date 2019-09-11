c0dfd4e752fe4dabbb9b5fd18e668f4ee638b2c8
Annakvn <38144067+Annakvn@users.noreply.github.com>
2019-09-11 00:24:50 -0700

    upload

---
+++ Book Solution-Modify/Book Project/MainForm.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TotalLabel = New System.Windows.Forms.Label()
+        Me.CalcButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.booksTextBox = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ErrorImage = Nothing
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(190, 146)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(230, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Number of books:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(230, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Total price:"
+        '
+        'TotalLabel
+        '
+        Me.TotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TotalLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TotalLabel.Location = New System.Drawing.Point(301, 59)
+        Me.TotalLabel.Name = "TotalLabel"
+        Me.TotalLabel.Size = New System.Drawing.Size(84, 25)
+        Me.TotalLabel.TabIndex = 5
+        Me.TotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CalcButton
+        '
+        Me.CalcButton.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CalcButton.Location = New System.Drawing.Point(233, 118)
+        Me.CalcButton.Name = "CalcButton"
+        Me.CalcButton.Size = New System.Drawing.Size(71, 27)
+        Me.CalcButton.TabIndex = 2
+        Me.CalcButton.Text = "&Calculate"
+        Me.CalcButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(314, 118)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(71, 27)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'booksTextBox
+        '
+        Me.booksTextBox.Location = New System.Drawing.Point(340, 22)
+        Me.booksTextBox.Name = "booksTextBox"
+        Me.booksTextBox.Size = New System.Drawing.Size(45, 23)
+        Me.booksTextBox.TabIndex = 1
+        Me.booksTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 178)
+        Me.Controls.Add(Me.booksTextBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.CalcButton)
+        Me.Controls.Add(Me.TotalLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "MainForm"
+        Me.Text = "Book Shack"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TotalLabel As Label
+    Friend WithEvents CalcButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents booksTextBox As TextBox
+End Class
---
+++ Book Solution-Modify/Book Project/MainForm.vb
@@ -0,0 +1,54 @@
+﻿' Book Project
+' Display the total price
+' Anna Kovneva March 11,2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class MainForm
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles booksTextBox.KeyPress
+        ' accept only numbers and the backspace
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
+        ' calculate the total price
+        Const Price1To3 As Double = 2.99
+        Const Price4To6 As Double = 2.49
+        Const PriceOver6 As Double = 1.75
+        Dim numBooks As Integer
+        Dim totalPrice As Double
+        Dim discount As Double = 0.1
+        Dim discountedPrice As Double
+        Dim button As DialogResult
+        Dim button2 As DialogResult
+
+        Integer.TryParse(booksTextBox.Text, numBooks)
+        Select Case numBooks
+            Case 1 To 3
+                totalPrice = Price1To3 * numBooks
+            Case 4 To 6
+                totalPrice = Price4To6 * numBooks
+            Case Is > 6
+                totalPrice = PriceOver6 * numBooks
+                button = MessageBox.Show("$1 coupon?", "Book Shack", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+                If button = DialogResult.Yes Then
+                    totalPrice -= 1
+                End If
+        End Select
+        button2 = MessageBox.Show("Are you a member of the Shack Book Club?", "Book Shack", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+        If button2 = DialogResult.Yes Then
+            discountedPrice = totalPrice * discount
+            totalPrice = totalPrice - discountedPrice
+        End If
+        TotalLabel.Text = totalPrice.ToString("C2")
+        booksTextBox.Focus()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
